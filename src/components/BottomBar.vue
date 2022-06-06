5f3dd9b57a553448ba2ee3c42348d454357ced29
Jan Kohlbach <jan@dorfjungs.com>
2022-06-06 14:55:50 +0200

    🔥✨💄 more initial setup

---
+++ src/components/BottomBar.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <nav>
+    <div class="grid">
+      <RouterLink to="/">calculate</RouterLink>
+      <RouterLink to="/convert">convert</RouterLink>
+    </div>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+@use "@/assets/styles/vars.scss" as *;
+@use "@/assets/styles/functions.scss" as *;
+
+nav {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  padding: rem(20) 0;
+  background-color: $color-secondary;
+}
+
+.grid {
+  display: flex;
+  justify-content: center;
+  align-self: start;
+}
+
+a {
+  padding: 0 rem(15);
+  color: #fff;
+  text-decoration: none;
+}
+</style>
---
+++ src/views/ConvertView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="grid">
+    <div class="content">this is the convert view</div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.content {
+  grid-column: 2 / span 10;
+}
+</style>
